e08d5c212cdec717c4430ff4cc84134c5be10740
huberap <huberap@rose-hulman.edu>
2018-05-17 08:32:40 -0400

    project working

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -25,6 +25,7 @@ class Snatch3r(object):
        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
        self.arm_motor = ev3.MediumMotor(ev3.OUTPUT_A)
        self.touch_sensor = ev3.TouchSensor()
+       self.color_sensor = ev3.ColorSensor()
 
        self.exit = 0
 
