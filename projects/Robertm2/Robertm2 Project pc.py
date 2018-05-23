93f1c011e92738e4e9ae2de5b662ebf4352496d4
robertml2 <robertm2@rose-hulman.edu>
2018-05-23 18:11:15 -0400

    commit

--- projects/Robertm2/Robertm2 Project pc.py
+++ projects/Robertm2/Robertm2 Project pc.py
@@ -27,12 +27,63 @@ def main():
 
     backwards_button = ttk.Button(main_frame, text='Backwards')
     backwards_button.grid()
-    backwards_entry = ttk.Entry(main_frame, width=8)
-    backwards_entry.grid(row= 2, column=1)
+    backwards_button['command'] = lambda: backwards(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<Down>', lambda event: backwards(mqtt_client, left_speed_entry, right_speed_entry))
+
+    right_button = ttk.Button(main_frame, text ='Right')
+    right_button.grid()
+    right_button['command'] = lambda: right(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<Right>', lambda event: right(mqtt_client, left_speed_entry, right_speed_entry))
+
+    left_button = ttk.Button(main_frame, text='Left')
+    left_button.grid()
+    left_button['command'] = lambda: left(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<Left>', lambda event: left(mqtt_client, left_speed_entry, right_speed_entry))
+
+    arm_up_button = ttk.Button(main_frame, text='Arm Up')
+    arm_up_button.grid()
+    arm_up_button['command'] = lambda: send_up(mqtt_client)
+    root.bind('<w>', lambda event: send_up(mqtt_client))
+
+    arm_down_button = ttk.Button(main_frame, text='Arm Down')
+    arm_down_button.grid()
+    arm_down_button['command'] = lambda: send_down(mqtt_client)
+    root.bind('<s>', lambda event: send_down(mqtt_client))
+
+    stop_button = ttk.Button(main_frame, text='Stop')
+    stop_button.grid()
+    stop_button['command'] = lambda: stop(mqtt_client)
+    root.bind('<space>', lambda event: stop(mqtt_client))
+
 
 def forward(mqtt_client, left_speed_entry, right_speed_entry):
     print('Forward is working')
     mqtt_client.send_message("forward_push", [int(left_speed_entry.get()), int(right_speed_entry.get())])
+
+def backwards(mqtt_client, left_speed_entry, right_speed_entry):
+    print('Backwards is working')
+    mqtt_client.send_message("backwards_push", [-int(left_speed_entry.get()), -int(right_speed_entry.get())])
+
+def right(mqtt_client, left_speed_entry, right_speed_entry):
+    print('Right is working')
+    mqtt_client.send_message("right_push", [int(left_speed_entry()), -int(right_speed_entry())])
+
+def left(mqtt_client, left_speed_entry, right_speed_entry):
+    print('Left is working')
+    mqtt_client.send_message("left_push", [-int(left_speed_entry()), int(right_speed_entry())])
+
+def send_up(mqtt_client):
+    print('Arm up is working')
+    mqtt_client.send_message("arm_up")
+
+def send_down(mqtt_client):
+    print('Arm down is working')
+    mqtt_client.send_message("arm_down")
+
+def stop(mqtt_client):
+    print('Stop is working')
+    mqtt_client.send_message("forward_push", [0,0])
+
 
 
 
